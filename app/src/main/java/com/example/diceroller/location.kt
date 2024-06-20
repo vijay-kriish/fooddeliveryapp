ef998ee19e700ba5573ea65b77f5fcbcbb35fc46
Vijayakrishnaji <ranvenyam@gmail.com>
2024-06-20 12:36:42 +0530

    menu card

--- app/src/main/java/com/example/diceroller/location.kt
+++ app/src/main/java/com/example/diceroller/location.kt
@@ -1,7 +1,9 @@
 package com.example.diceroller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.example.diceroller.databinding.ActivityLocationBinding
@@ -18,6 +20,11 @@ class location : AppCompatActivity() {
         val adapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,location)
         val autoCompleteTextView=binding.listt
         autoCompleteTextView.setAdapter(adapter)
+        Handler().postDelayed({
+            val intent = Intent(this, com.example.diceroller.menu::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
 
     }
 }
